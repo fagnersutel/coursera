7b60d7507d3a563083446fec6968682c115a3293
Fagner Sutel <fagnersutel@hotmail.coml>
2018-01-23 20:50:44 -0200

    segundo commit

--- eda/ex1/Plot1.R
+++ eda/ex1/Plot1.R
@@ -1,13 +1,16 @@
 setwd("../Exploratory Data Analysis/week4/coursera/eda/ex1/")
 
 summarySCC_PM25 <- readRDS("FNEI_data/summarySCC_PM25.rds")
-SCC <- readRDS("FNEI_data/Source_Classification_Code.rds")
+Source_Classification_Code <- readRDS("FNEI_data/Source_Classification_Code.rds")
 
 totalNEI <- aggregate(Emissions ~ year, summarySCC_PM25, sum)
 par(mfrow=c(1,2))
+png("Plot1.png", width=480, height=480)
 plot(totalNEI$year, ((totalNEI$Emissions)/10^6), type = "o", col = "springgreen3", 
      main = expression("Emissoes PM2.5 por Ano nos EUA"), ylab = expression("Emissoes nos EUA de PM2.5 (10^6 Toneladas)"), xlab = "Ano")
+dev.off()
 
+png("Plot1b.png", width=480, height=480)
 barplot(
     ((totalNEI$Emissions)/10^6),
     totalNEI$year,
@@ -15,3 +18,4 @@ barplot(
     ylab="Emissoes nos EUA de PM2.5 (10^6 Toneladas)",
     main="Emissoes PM2.5 por Ano nos EUA"
 )
+dev.off()
