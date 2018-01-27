24b313c7fc2fb3537cd026801e594b650dce3df0
Fagner Sutel <fagnersutel@hotmail.coml>
2018-01-27 14:57:33 -0200

    27/01/2017 - 14:57

--- eda/ex1/Plot6.R
+++ eda/ex1/Plot6.R
@@ -49,7 +49,8 @@ ggplot(cidade_veiculos, aes(x=year, y=Emissions, fill=fips)) +
     geom_bar(aes(fill=year),stat="identity") +
     facet_grid(.~fips) +
     guides(fill=FALSE) + theme_bw() +
-    labs(x="Anos", y=expression("Emissoes de PM2.5 (Toneladas)")) + 
-    labs(title=expression("PM"[2.5]*" Emissoes Anuais de PM2.5 por Veiculos em Baltimore e Los Angeles"))
+    labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+    labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))  +
+    xlim(1996, 2011)
 dev.off()
     
